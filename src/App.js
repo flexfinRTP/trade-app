73ddba7a787b2dddeb90190d8b3c9f008f5becef
flexfinRTP <gaffney311@outlook.com>
2021-05-24 21:59:36 -0500

    header and record comp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Record from './Record';
+
 
 function App() {
   return (
@@ -7,17 +10,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            React Trade App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <table>
+        <Header onSearch={this.searchRecords} onSort={this.onSort} />
+        <Record records={this.state.records} />
+      </table>
     </div>
   );
 }
